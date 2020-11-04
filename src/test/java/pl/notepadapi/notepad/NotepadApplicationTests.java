52c38765fbe46108fdd27f30d206e1a312e55612
Faron Norbert <faronnorbertkrk@gmail.com>
2020-11-04 21:50:47 +0100

    initial

---
+++ src/test/java/pl/notepadapi/notepad/NotepadApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.notepadapi.notepad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotepadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
